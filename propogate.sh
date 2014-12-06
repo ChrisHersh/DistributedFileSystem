6d495a4c8d8bbb7dfaf51a1abdc03dfa5cbc42d7
ChrisHersh <ch5968@cs.ship.edu>
2014-12-05 22:28:48 -0500

    Created a bash script that, on sloop, will scp the ~/git contents to each Amazon server

---
+++ propogate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+scp -r -i Ship.pem ~/git p1090@clientOne:~/
+ 
+scp -r -i Ship.pem ~/git p1090@clientTwo:~/
+ 
+scp -r -i Ship.pem ~/git p1090@clientThree:~/
+ 
+scp -r -i Ship.pem ~/git p1090@dfsJapan:~/
+ 
+scp -r -i Ship.pem ~/git p1090@dfsIreland:~/
+ 
+scp -r -i Ship.pem ~/git p1090@dfsVirginia:~/
+
+
